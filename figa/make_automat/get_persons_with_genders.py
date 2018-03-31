3d3797d71d15081b8515344d752ae1fb0b872bde
Vlczech (Volf Tomáš) <ivolf@fit.vutbr.cz>
2018-03-31 17:04:49 +0200

    Revert separation of invalid genders from get_persons_with_genders

--- figa/make_automat/get_persons_with_genders.py
+++ figa/make_automat/get_persons_with_genders.py
@@ -13,7 +13,7 @@ headKB = metrics_knowledge_base.getDictHeadKB()
 # multiple values delimiter
 KB_MULTIVALUE_DELIM = metrics_knowledge_base.KB_MULTIVALUE_DELIM
 
-def generate_name_alternatives(kb_path, valid_only):
+def generate_name_alternatives(kb_path):
     if kb_path:
         name_lines = []
         with open(kb_path) as kb:
@@ -29,16 +29,15 @@ def generate_name_alternatives(kb_path, valid_only):
 
                         gender = line[7]
 
-                        if (gender in ['M', 'F']) == valid_only:
-                            for t in aliases:
-                                t = re.sub('\s+', ' ', t).strip()
-                                unsuitable = ";?!()[]{}<>/~@#$%^&*_=+|\"\\"
-                                t = t.strip()
-                                for x in unsuitable:
-                                    if x in t:
-                                        break
-                                else:
-                                    name_lines.append(t + '\t' + gender)
+                        for t in aliases:
+                            t = re.sub('\s+', ' ', t).strip()
+                            unsuitable = ";?!()[]{}<>/~@#$%^&*_=+|\"\\"
+                            t = t.strip()
+                            for x in unsuitable:
+                                if x in t:
+                                    break
+                            else:
+                                name_lines.append(t + '\t' + gender)
 
         for n in name_lines:
             print(n)
@@ -46,7 +45,6 @@ def generate_name_alternatives(kb_path, valid_only):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('-p', '--kb_path')
-    parser.add_argument('--invalid', action="store_true")
     args = parser.parse_args()
 
-    generate_name_alternatives(args.kb_path, not args.invalid)
+    generate_name_alternatives(args.kb_path)
